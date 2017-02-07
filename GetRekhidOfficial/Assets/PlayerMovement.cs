39e0b8e98bc8d2d3f8a64484fc7e54e4ee941a4e
Alex <aborman7@gmail.com>
2017-02-06 20:20:33 -0500

    Basic movement
    -added drag
    -added player 2 movement (P1 is WASD, P2 is arrow keys)

--- GetRekhidOfficial/Assets/PlayerMovement.cs
+++ GetRekhidOfficial/Assets/PlayerMovement.cs
@@ -2,34 +2,24 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-//Taken from Unity's 2d UFO Tutorial
 
-	public float speed;		//Store's player's movement speed
+	public float accel;	
+    public float friction;
 
-	private Rigidbody2D rb2d;	//Store a referance to the Rigidbody2D
+    private Rigidbody2D player1;
 
-	// Use this for initialization
-	void Start () {
-
-		//get and store Rigidbody referance
-		rb2d = GetComponent<Rigidbody2D> ();
+    // Use this for initialization
+    void Start () {
+        player1 = GetComponent<Rigidbody2D> ();
+        player1.drag = friction;
+    }
 	
-	}
-	
-	// FixedUpdate is called at a fixed interval independent of frame rate
-	//Physics code goes here
 	void FixedUpdate () {
-
-		//store current horizontal input in float moveHorizontal
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-
-		//store current vertical input in the float moveVertical
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		//Ust the stored floats to creat a new Vector2 variable movement
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 
-		//call AddForce function to rigidbody and multiply by speed
-		rb2d.AddForce (movement * speed);	
+        player1.AddForce(movement * accel);
 	}
 }
