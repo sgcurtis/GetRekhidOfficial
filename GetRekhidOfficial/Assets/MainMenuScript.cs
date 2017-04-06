da7f1d9a1daecf4c840071000abdf6e1b444dd8e
Alex <aborman7@gmail.com>
2017-04-05 20:44:06 -0400

    MenuAndKeybinds
    - Finished main menu
    - Added keybind screen
    - Changed input system to use new keybinds
    - Fixed git merge error (hopefull)

---
+++ GetRekhidOfficial/Assets/MainMenuScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public Canvas mainMenu;
+    public Canvas quitMenu;
+    public Canvas keybindMenu;
+
+	// Use this for initialization
+	void Start () {
+        mainMenu = mainMenu.GetComponent<Canvas>();
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        mainMenu.enabled = true;
+        quitMenu.enabled = false;
+        keybindMenu.enabled = false;
+
+    }
+	
+	public void QuitPress()
+    {
+        mainMenu.enabled = false;
+        quitMenu.enabled = true;
+    }
+
+    public void CancelPress()
+    {
+        mainMenu.enabled = true;
+        quitMenu.enabled = false;
+    }
+
+    public void YesPress()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void KeybindsPress()
+    {
+        mainMenu.enabled = false;
+        keybindMenu.enabled = true;
+    }
+
+    public void KeybindsExit()
+    {
+        mainMenu.enabled = true;
+        keybindMenu.enabled = false;
+    }
+
+}
---
+++ GetRekhidOfficial/Assets/PauseMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Canvas pauseMenu;
+    public Canvas quitMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        pauseMenu = pauseMenu.GetComponent<Canvas>();
+        pauseMenu.enabled = false;
+        quitMenu.enabled = false;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseMenu.enabled && !quitMenu.enabled)
+            {
+                Time.timeScale = 0;
+                pauseMenu.enabled = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseMenu.enabled = false;
+                quitMenu.enabled = false;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.enabled = false;
+        Time.timeScale = 1;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitPress()
+    {
+        pauseMenu.enabled = false;
+        quitMenu.enabled = true;
+    }
+
+    public void QuitToMainMenuPress()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void CancelPress()
+    {
+        pauseMenu.enabled = true;
+        quitMenu.enabled = false;
+    }
+
+    public void QuitToDesktopPress()
+    {
+        Application.Quit();
+    }
+
+   
+}
