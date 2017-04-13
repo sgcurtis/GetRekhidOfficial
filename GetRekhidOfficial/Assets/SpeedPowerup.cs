98c6acc33abe41803dab0cf447932bc4e5e45910
Lucus <ljbauder@mtu.edu>
2017-04-12 21:09:24 -0400

    Revert "Revert "Powerups Update 2""
    This reverts commit 006fa28b4267780c3a9c07e0f157710a2f83b595.

--- GetRekhidOfficial/Assets/SpeedPowerup.cs
+++ GetRekhidOfficial/Assets/SpeedPowerup.cs
@@ -9,41 +9,56 @@ public class SpeedPowerup : MonoBehaviour {
 	public Rigidbody2D Player1C;
 	public Rigidbody2D Player2C;
 	public bool Spawned;
+	public double spawnTimer;
 
 	// Use this for initialization
 	void Start () {
 		Player1C = Player1.GetComponent<Rigidbody2D> ();
 		Player2C = Player2.GetComponent<Rigidbody2D> ();
 		SpdPwr = GameObject.Find ("SpeedPowerup");
-		Spawned = false;
+		Random.InitState ( System.DateTime.Now.Millisecond);
+		despawn ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (!Spawned) {
+		spawnTimer += Time.deltaTime;
+		if (!Spawned && spawnTimer > 5) {
 			spawn ();
+		}
+		if (Spawned) {
+			int y = Random.Range ( 1, 256 );
+			SpdPwr.GetComponent<Rigidbody2D>().AddForce(new Vector3(Mathf.Sin(y % (2 * Mathf.PI)),y % 5));
+			if (spawnTimer > 20) {
+				despawn ();
+			}
 		}
 	}
 
+	void despawn() {
+		spawnTimer = 0;
+		Spawned = false;
+		SpdPwr.transform.Translate (10000, 10000, 0, Space.World);
+	}
+
 	void spawn() {
-		int x = (int) Random.value * 100;
-		int y = (int) Random.value * 100;
-		SpdPwr.transform.position = new Vector3(x, y);
+		int y = (int) (Random.value * 2);
+		SpdPwr.transform.position = new Vector3 (3 * Mathf.Sqrt(y), -8);
+		Player2.GetComponent<PlayerMovement> ().accel = 200;
+		Player1.GetComponent<PlayerMovement> ().accel = 200;
 		Spawned = true;
 	}
 
 	//10 sec
 	void OnTriggerEnter2D( Collider2D collider2D ) {
 		if (collider2D.gameObject.Equals (Player2)) {
-			Player2.GetComponent<PlayerMovement>().accel = 500;
+			Player2.GetComponent<PlayerMovement>().accel = 350;
 			Debug.Log ("PLAYER 1 SHOULD BE SPEEDY");
-			SpdPwr.transform.position = new Vector3 (1000, 1000);
-			Spawned = false;
+			despawn ();
 		} else if (collider2D.gameObject.Equals (Player1)) {
-			Player1.GetComponent<PlayerMovement>().accel = 500;
+			Player1.GetComponent<PlayerMovement>().accel = 350;
 			Debug.Log ("PLAYER 2 SHOULD BE SPEEDY");
-			SpdPwr.transform.position = new Vector3 (1000, 1000);
-			Spawned = false;
+			despawn ();
 		}
 	}
 }
--- GetRekhidOfficial/Assets/StrengthPowerup.cs
+++ GetRekhidOfficial/Assets/StrengthPowerup.cs
@@ -9,26 +9,43 @@ public class StrengthPowerup : MonoBehaviour {
 	public Rigidbody2D Player1C;
 	public Rigidbody2D Player2C;
 	public bool Spawned;
+	public double spawnTimer;
 
 	// Use this for initialization
 	void Start () {
 		Player1C = Player1.GetComponent<Rigidbody2D> ();
 		Player2C = Player2.GetComponent<Rigidbody2D> ();
 		StrPwr = GameObject.Find ("StrengthPowerup");
-		Spawned = false;
+		Random.InitState ( System.DateTime.Now.Millisecond * 2);
+		despawn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!Spawned) {
+		spawnTimer += Time.deltaTime;
+		if (!Spawned && spawnTimer > 5) {
 			spawn ();
+		}
+		if (Spawned) {
+			Random.InitState ( System.DateTime.Now.Millisecond * 2);
+			StrPwr.GetComponent<Rigidbody2D>().AddForce(new Vector3(1, Mathf.Sin(Random.value * Mathf.PI) / 2));
+			if (spawnTimer > 20) {
+				despawn ();
+			}
 		}
 	}
 
+	void despawn() {
+		spawnTimer = 0;
+		Spawned = false;
+		StrPwr.transform.Translate (10000, -10000, 0, Space.World);
+	}
+
 	void spawn() {
-		int x = (int) Random.value * 100;
-		int y = (int) Random.value * 100;
-		StrPwr.transform.position = new Vector3(x, y);
+		int y = (int) (Random.value * 2);
+		StrPwr.transform.position = new Vector3 (0, 0);
+		Player2.GetComponent<PlayerMovement> ().accel = 200;
+		Player1.GetComponent<PlayerMovement> ().accel = 200;
 		Spawned = true;
 	}
 
@@ -38,14 +55,12 @@ public class StrengthPowerup : MonoBehaviour {
 			//Player1C.sharedMaterial.bounciness = 100;
 			Player2C.mass = 100;
 			Debug.Log ("PLAYER 1 SHOULD BE BOUNCY");
-			StrPwr.transform.position = new Vector3 (1000, 1000);
-			Spawned = false;
+			despawn ();
 		} else if (collider2D.gameObject.Equals (Player1)) {
 			//Player2C.sharedMaterial.bounciness = 100;
 			Player1C.mass = 100;
 			Debug.Log ("PLAYER 2 SHOULD BE BOUNCY");
-			StrPwr.transform.position = new Vector3 (1000, 1000);
-			Spawned = false;
+			despawn ();
 		}
 	}
 }
